3ba4b565ba34f8b09f990baff0d687aa754ee9c6
Oleg <pristup2005@gmail.com>
2021-02-16 15:08:29 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from time import sleep
+
+from telethon import TelegramClient
+import configparser
+config = configparser.ConfigParser()
+config.read("config.ini", encoding="utf-8")
+
+api_id = int(config['Resender']['api_id'])
+api_hash = config['Resender']['api_hash']
+my_channel = config['Resender']['channel_to_send']
+client = TelegramClient('resender', api_id, api_hash)
+message = config['Resender']['message']
+
+client.start()
+
+
+
+async def main():
+    while True:
+        await client.send_message(my_channel, message)
+        sleep(7)
+
+with client:
+    client.loop.run_until_complete(main())
